767eda349573bbf35427a191bb6c38f2308672d6
dupuy <aurelie.dupuy2021@campus-eni.fr>
2022-02-21 19:54:44 +0100

    Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EnTeteComponent } from './en-tete/en-tete.component';
+import { PiedDePageComponent } from './pied-de-page/pied-de-page.component';
+import { CorpsComponent } from './corps/corps.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EnTeteComponent,
+    PiedDePageComponent,
+    CorpsComponent
   ],
   imports: [
     BrowserModule,
